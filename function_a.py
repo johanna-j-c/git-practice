5cce27da0b543ae9c2486804641a9a18bfee9374
Jessica <Yiskah.Scientia@gmail.com>
2023-03-24 15:25:00 -0600

    test

--- function_a.py
+++ function_a.py
@@ -1,22 +1,9 @@
-def most_common_value(number_list):
-    """ returns the most common element of the list
+def merge_lists(list_a, list_b):
+    """ Returns a new list which is
+        a combination of list_a and list_b
+        without any duplicate elements.
     """
-    """ reads numbers from the user
-        summing as we go until either
-        the user enters 0, or
-        the sum reaches or exceeds 1000
-    """
-    num = int(input('Please enter a number => '))
-    sum = 0
-
-    while num != 0:
-        sum += num
-        if sum >= 1000:
-            break
-
-        num = int(input('Please enter a number => '))
-
-    return sum
+    return list(set(list_a) | set(list_b))
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
